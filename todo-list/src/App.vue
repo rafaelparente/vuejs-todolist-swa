8879d25a8f389ad1bd00c9025266fbb49f8bfe4c
Rafael Parente <rafaelp.dev@gmail.com>
2020-08-02 19:15:40 -0300

    Atualizando o arquivo 'App.vue'

--- todo-list/src/App.vue
+++ todo-list/src/App.vue
@@ -3,7 +3,7 @@
     <div class="ui inverted vertical masthead center aligned segment">
       <div class="ui text container">
         <h1 class="ui inverted header"> Aplicação Todo List com Vue.js 3</h1>
-        <h2 class="ui inverted header">Glaucia Lemos
+        <h2 class="ui inverted header">Rafael Parente
           <div class="sub header">Cloud Advocate em JavaScript/Node.js @ Microsoft</div>
         </h2>
         <h3 class="ui inverted header">Rio de Janeiro, Brasil - 2020</h3>
